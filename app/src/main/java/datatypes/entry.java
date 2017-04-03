edb1105d289086951ed2f6196afb7f4cc3e043cb
Johannes Matthias Koehn <johannes.matthias.koehn@freenet.de>
2017-04-03 11:02:23 +0200

    Test-Commit Android Studio

---
+++ app/src/main/java/datatypes/entry.java
@@ -0,0 +1,22 @@
+package datatypes;
+
+/**
+ * Created by Hannes on 03.04.2017.
+ */
+
+public class entry {
+    private String v_name;
+    private String v_borrowedBy;
+
+
+
+
+
+
+    public void Entry(String p_name, String p_borrowedBy){
+        this.v_name = p_name;
+        this.v_borrowedBy = p_borrowedBy;
+    }
+
+
+}
